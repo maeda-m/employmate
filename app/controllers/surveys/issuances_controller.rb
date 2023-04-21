d0aafa69794f4a87b42be87f1dd88c45d73d28c8
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-04-22 08:01:47 +0900

    リソース設計に基づいて足場となる空ファイルを作成した
    あわせて rubocop を見直しした

---
+++ app/controllers/surveys/issuances_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class Surveys::IssuancesController < ApplicationController
+  def create
+  end
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def terms_of_service
+  end
+
+  def privacy_policy
+  end
+
+  def robots
+  end
+end
