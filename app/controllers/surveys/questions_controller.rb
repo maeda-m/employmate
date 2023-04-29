dd8f727ca917aa72e01bdece1c40e416d54e9198
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-04-29 21:57:00 +0900

    次へと戻るボタンの制御をレンダリングのインラインで対応した

--- app/controllers/surveys/questions_controller.rb
+++ app/controllers/surveys/questions_controller.rb
@@ -2,29 +2,36 @@
 
 class Surveys::QuestionsController < ApplicationController
   def next
-    question = Survey.find(params[:survey_id]).next_question(params[:id])
+    current_question = Question.find(params[:id])
+    next_question = Survey.find(params[:survey_id]).next_question(current_question)
 
-    if question
-      streams = [
-        turbo_stream.append('survey-profiles', partial: 'show_question', locals: { question: })
-      ]
+    streams = [
+      turbo_stream.append(current_question, partial: 'hide_question', locals: { question: current_question })
+    ]
 
-      if question.answer_component.overtime?
+    if next_question
+      streams << turbo_stream.append(next_question, partial: 'show_question', locals: { question: next_question })
+
+      if next_question.answer_component.overtime?
         prev_answer = params.require(:answers).fetch(params[:id])[:value]
-        streams << turbo_stream.append(question, partial: '/components/overtime_field/show_field', locals: { question:, prev_answer: })
+        streams << turbo_stream.append(next_question, partial: '/components/overtime_field/show_field', locals: { question: next_question, prev_answer: })
       end
-
-      render turbo_stream: streams
     else
-      render turbo_stream: turbo_stream.append('survey-profiles', partial: 'show_submit')
+      streams << turbo_stream.append('survey-profiles', partial: 'show_submit')
     end
+
+    render turbo_stream: streams
   end
 
   def back
-    question = Survey.find(params[:survey_id]).prev_question(params[:id])
+    current_question = Question.find(params[:id])
+    prev_question = Survey.find(params[:survey_id]).prev_question(current_question)
 
-    if question
-      render turbo_stream: turbo_stream.append('survey-profiles', partial: 'show_question', locals: { question: })
+    if prev_question
+      render turbo_stream: [
+        turbo_stream.append(current_question, partial: 'hide_question', locals: { question: current_question }),
+        turbo_stream.append(prev_question, partial: 'show_question', locals: { question: prev_question })
+      ]
     else
       render turbo_stream: turbo_stream.append('survey-profiles', partial: 'visit_welcome')
     end
--- app/javascript/controllers/survey_profiles_controller.js
+++ app/javascript/controllers/survey_profiles_controller.js
@@ -2,40 +2,42 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   initialize() {
-    this.element.querySelectorAll('turbo-frame')[0].hidden = false
+    this.originFormAction = this.form.action
+    this.originFormValidate = this.form.noValidate
+    this.form.querySelectorAll('turbo-frame')[0].hidden = false
   }
 
   get form() {
-    return this.element.querySelector('form')
+    return this.element
+  }
+
+  restoreOriginFormAction() {
+    this.form.action = this.originFormAction
+    this.form.noValidate = this.originFormValidate
   }
 
   frame(event) {
     return event.target.closest('turbo-frame')
   }
 
-  hide(event) {
-    this.frame(event).hidden = true
+  showNextQuestionAction(event) {
+    return this.frame(event).dataset.formaction
   }
 
-  hideWithValid(event) {
+  showNextQuestionWithCurrentQuestionValid(event) {
     const fields = this.frame(event).querySelectorAll('input,select')
     const isValid = Array.from(fields).every((field) => {
       return field.reportValidity()
     })
 
     if (isValid) {
-      this.hide(event)
-      this.form.action = this.frame(event).dataset.formaction
-    } else {
-      event.preventDefault()
+      this.showNextQuestionWithoutCurrentQuestionValid(event)
     }
   }
 
-  hideWithInput(event) {
-    this.hide(event)
-    this.form.action = this.frame(event).dataset.formaction
+  showNextQuestionWithoutCurrentQuestionValid(event) {
+    this.form.action = this.showNextQuestionAction(event)
     this.form.noValidate = true
     this.form.requestSubmit()
-    this.form.noValidate = false
   }
 }
