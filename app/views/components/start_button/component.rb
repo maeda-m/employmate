17337d96729deab283de4c387c8f0d4cdedb2e98
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-04-29 09:20:12 +0900

    はじめるボタンとやりなおすボタンをコンポーネント化した

--- app/views/components/start_button/component.rb
+++ app/views/components/start_button/component.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class StartButton::Component < ApplicationViewComponent
+  attr_reader :caption
+
+  def initialize(caption:)
+    super
+    @caption = caption
+  end
+
   def survey_id
     Survey.profiles.first.id
   end
