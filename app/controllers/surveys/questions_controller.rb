a2213a0bff1d993ddbb597a5aa4788aeb23d6c6f
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-04-29 09:14:17 +0900

    質問ページのフォームで整数を入力できるようにした

--- app/controllers/surveys/questions_controller.rb
+++ app/controllers/surveys/questions_controller.rb
@@ -3,6 +3,7 @@
 class Surveys::QuestionsController < ApplicationController
   def next
     @question = Survey.find(params[:survey_id]).next_question(params[:id])
+    @current_answer_value = params.require(:answers).fetch(params[:id])[:value]
   end
 
   def back
--- app/views/components/overtime_field/component.rb
+++ app/views/components/overtime_field/component.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class OvertimeField::Component < AnswerFieldComponent
+  def self.generate_labels(numeric)
+    raise ArgumentError unless /\A\d{4}-\d{1,2}\z/.match?(numeric)
+
+    end_month = Date.parse("#{numeric}-01")
+    months = Array.new(6) { |i| end_month - i.months }.reverse
+
+    months.map { |m| I18n.localize(m, format: '%Y/%m') }
+  end
 end
