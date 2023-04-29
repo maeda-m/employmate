51e0e784f1591f0c7d8817e9fe8b4bac94bd7ded
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-04-29 20:37:27 +0900

    View 側で条件分岐していたレンダリングをインラインにした

--- app/controllers/surveys/questions_controller.rb
+++ app/controllers/surveys/questions_controller.rb
@@ -2,11 +2,31 @@
 
 class Surveys::QuestionsController < ApplicationController
   def next
-    @question = Survey.find(params[:survey_id]).next_question(params[:id])
-    @current_answer_value = params.require(:answers).fetch(params[:id])[:value]
+    question = Survey.find(params[:survey_id]).next_question(params[:id])
+
+    if question
+      streams = [
+        turbo_stream.append('survey-profiles', partial: 'show_question', locals: { question: })
+      ]
+
+      if question.answer_component.overtime?
+        prev_answer = params.require(:answers).fetch(params[:id])[:value]
+        streams << turbo_stream.append(question, partial: '/components/overtime_field/show_field', locals: { question:, prev_answer: })
+      end
+
+      render turbo_stream: streams
+    else
+      render turbo_stream: turbo_stream.append('survey-profiles', partial: 'show_submit')
+    end
   end
 
   def back
-    @question = Survey.find(params[:survey_id]).prev_question(params[:id])
+    question = Survey.find(params[:survey_id]).prev_question(params[:id])
+
+    if question
+      render turbo_stream: turbo_stream.append('survey-profiles', partial: 'show_question', locals: { question: })
+    else
+      render turbo_stream: turbo_stream.append('survey-profiles', partial: 'visit_welcome')
+    end
   end
 end
