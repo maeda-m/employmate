e0a8ab2e85fe8555aa1245e2da98501d575699dc
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-05-02 11:38:30 +0900

    質問と回答結果を紐づけるカラムを追加した

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,5 @@
 class Question < ApplicationRecord
   belongs_to :questionnaire
   belongs_to :answer_component, foreign_key: :answer_component_type, primary_key: :type
+  belongs_to :answer_gateway, foreign_key: :answer_gateway_rule, primary_key: :rule, optional: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_01_115318) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_01_115534) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_01_115318) do
     t.string "answer_component_type", null: false
     t.boolean "required", default: false
     t.integer "position", null: false
+    t.string "answer_gateway_rule"
     t.index ["questionnaire_id"], name: "index_questions_on_questionnaire_id"
   end
 
