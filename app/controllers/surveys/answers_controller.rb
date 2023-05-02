4dde497121f9662b02a9326e727343914209fec0
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-05-02 11:57:38 +0900

    HTMLからの回答内容をコントローラで明示的に指定した

--- app/controllers/surveys/answers_controller.rb
+++ app/controllers/surveys/answers_controller.rb
@@ -2,6 +2,17 @@
 
 class Surveys::AnswersController < ApplicationController
   def create
-    redirect_to user_profiles_url(user_id: 'TODO')
+    survey = Survey.find(params[:survey_id])
+
+    raise NotImplementedError if survey.type_of_profile?
+
+    redirect_to user_profile_url(user_id: 'TODO')
+  end
+
+  private
+
+  def answers_params
+    params.require(:answers)
+    params.permit(answers: [:value, { values: %w[0 1 2 3 4 5] }])[:answers]
   end
 end
--- app/views/components/overtime_field/component.rb
+++ app/views/components/overtime_field/component.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class OvertimeField::Component < AnswerFieldComponent
+  FIELD_COUNT = 6
+
   def self.generate_labels(numeric)
     raise ArgumentError unless /\A\d{4}-\d{1,2}\z/.match?(numeric)
 
     end_month = Date.parse("#{numeric}-01")
-    months = Array.new(6) { |i| end_month - i.months }.reverse
+    months = Array.new(FIELD_COUNT) { |i| end_month - i.months }.reverse
 
     months.map { |m| I18n.localize(m, format: '%Y/%m') }
   end
