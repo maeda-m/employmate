8a6f579741da54f730452b867a57fd25bc4bad4f
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-05-20 08:56:23 +0900

    暫定または確定した失業認定日の算定を実装した

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,34 @@
 
 class Profile < ApplicationRecord
   belongs_to :user
+
+  def after_unemployed_on
+    unemployed_on.next_day
+  end
+
+  def issuanced_on_of_release_form
+    after_unemployed_on + 10.days
+  end
+
+  def draft_explanitory_seminar_on_for_employment_insurance
+    issuanced_on_of_release_form + 7.national_gov_org_weekdays
+  end
+
+  def first_unemployment_certification_on
+    fixed_first_unemployment_certification_on.presence ||
+      draft_unemployment_certification_on
+  end
+
+  def draft_unemployment_certification_on
+    beginning_day = draft_explanitory_seminar_on_for_employment_insurance + benefit_restriction_period
+    UnemploymentCertificationDay.new(beginning_day:).to_date
+  end
+
+  def benefit_restriction_period
+    return 0.days if unemployed_with_special_eligible?
+    return 0.days if unemployed_with_special_reason?
+
+    # TODO: [ 11, 12, 21, 22, 23, 31, 32, 33, 34 ].include?(reason_code_for_loss_of_employment)
+    2.months
+  end
 end
