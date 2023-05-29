16c7411e33e6e909ded4941940396165235f7a90
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-05-29 10:10:31 +0900

    CD設定を追加した

---
+++ .caprover/captain-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# See: https://docs.docker.com/samples/rails/
+mkdir -p /opt/rails/tmp/pids
+rm -f /opt/rails/tmp/pids/server.pid
+
+bundle install
+rails db:migrate db:seed
+rails assets:precompile
+
+exec "$@"
