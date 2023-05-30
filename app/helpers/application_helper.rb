dc44782750efe5ce62cbb65d52fdd621c4f2e3c7
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-05-30 22:11:18 +0900

    ウェルカムページの要素の配置を調整した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
   def caption_with_back_icon
     tag.span('戻る', class: 'fa-solid fa-chevron-left')
   end
+
+  def link_to_terms_of_service
+    link_to('利用規約', terms_of_service_path, target: '_blank', rel: 'noopener')
+  end
+
+  def link_to_privacy_policy
+    link_to('プライバシーポリシー', privacy_policy_path, target: '_blank', rel: 'noopener')
+  end
 end
