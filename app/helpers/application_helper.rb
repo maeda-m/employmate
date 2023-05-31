82c3e7ad2b7f208fade0b387b3e2b2476e827495
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-05-31 16:35:02 +0900

    補足文章要素をヘルパー化した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,18 @@ module ApplicationHelper
     controller_path.sub('/', '-')
   end
 
+  def help_message(title: nil)
+    tag.div(class: 'help') do
+      header = tag.h4 do
+        concat(tag.i(class: 'fa-solid fa-circle-info'))
+        concat(title)
+      end
+
+      concat(header)
+      yield if block_given?
+    end
+  end
+
   def caption_with_back_icon
     tag.span('戻る', class: 'fa-solid fa-chevron-left')
   end
--- app/views/components/task_schedule/component.rb
+++ app/views/components/task_schedule/component.rb
@@ -10,26 +10,26 @@ class TaskSchedule::Component < ApplicationViewComponent
     [
       @user.tasks.new(task_category: TaskCategory.first, title: 'ハローワークで就労可否の証明書を入手する'),
       @user.tasks.new(task_category: TaskCategory.first, title: '求職者マイページアカウント登録をする'),
-      @user.tasks.new(task_category: TaskCategory.third, title: "離職票をハローワークに提出する\n#{help_message}"),
+      @user.tasks.new(task_category: TaskCategory.third, title: "離職票をハローワークに提出する\n#{required_items.join("\n")}"),
       @user.tasks.new(task_category: TaskCategory.fourth, title: 'ハローワークで雇用保険説明会に出席する'),
       @user.tasks.new(task_category: TaskCategory.fifth, title: 'ハローワークで失業認定申告書を提出する'),
       @user.tasks.new(task_category: TaskCategory.sixth, title: '初回の給付金振込予定日です')
     ]
   end
 
-  def help_message
+  def required_items
     user_profile = @user.profile
 
-    messages = [
+    items = [
       '【持ち物】',
       '・離職票１，２',
       '・マイナンバーカード',
       '・キャッシュカード'
     ]
-    messages << '・残業時間の記載がある勤務実績表や給与明細書' if user_profile.unemployed_with_special_eligible?
-    messages << '・医師の診断書（発病や負傷時、退職日前後のもの）' if user_profile.unemployed_with_special_reason?
-    messages << '・就労可否の証明書' if user_profile.unemployed_with_special_reason?
+    items << '・残業時間の記載がある勤務実績表や給与明細書' if user_profile.unemployed_with_special_eligible?
+    items << '・医師の診断書（発病や負傷時、退職日前後のもの）' if user_profile.unemployed_with_special_reason?
+    items << '・就労可否の証明書' if user_profile.unemployed_with_special_reason?
 
-    messages.join("\n")
+    items
   end
 end
