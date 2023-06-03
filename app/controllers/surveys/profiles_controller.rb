59d4fa4511df29308fa892a23b2c202950042c97
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-06-03 17:20:55 +0900

    初回分析調査回答結果ページからやりなおしした際に戻れるようにした

--- app/controllers/surveys/profiles_controller.rb
+++ app/controllers/surveys/profiles_controller.rb
@@ -9,5 +9,10 @@ class Surveys::ProfilesController < ApplicationController
     survey = Survey.profiles.find(params[:survey_id])
     @answer = Answer.new(survey:)
     @questions = survey.all_questions
+    @start_page_path = if params[:started_by] == StartButton::Component::STARTED_BY_REDO
+                         user_profile_url(user_id: Current.user.id)
+                       else
+                         root_path
+                       end
   end
 end
--- app/views/components/start_button/component.rb
+++ app/views/components/start_button/component.rb
@@ -1,11 +1,20 @@
 # frozen_string_literal: true
 
 class StartButton::Component < ApplicationViewComponent
-  attr_reader :caption
+  STARTED_BY_TOP = 'welcome'
+  STARTED_BY_REDO = 'redo_answer'
 
-  def initialize(caption:)
+  CAPTIONS = {
+    STARTED_BY_TOP => 'はじめる',
+    STARTED_BY_REDO => 'やりなおす'
+  }.freeze
+
+  attr_reader :caption, :started_by
+
+  def initialize(context:)
     super
-    @caption = caption
+    @caption = CAPTIONS[context]
+    @started_by = context
   end
 
   def survey_id
