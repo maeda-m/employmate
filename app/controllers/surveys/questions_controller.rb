a7b18c6488135311035955de2f99bd85f7178366
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-06-16 16:56:34 +0900

    multiple turbo streams の条件分岐を整理した

--- app/controllers/surveys/questions_controller.rb
+++ app/controllers/surveys/questions_controller.rb
@@ -9,22 +9,14 @@ class Surveys::QuestionsController < ApplicationController
     current_question = Question.find(params[:id])
     next_question = Survey.find(params[:survey_id]).next_question(current_question, answer_values_without_next_questions(current_question))
 
-    streams = [
-      turbo_stream.append(current_question, partial: 'hide_question', locals: { question: current_question })
-    ]
-
     if next_question
-      streams << turbo_stream.append(next_question, partial: 'show_question', locals: { question: next_question })
-
-      if next_question.answer_component.overtime?
-        prev_answer = params.require(:answers).fetch(params[:id])[:value]
-        streams << turbo_stream.append(next_question, partial: '/components/overtime_field/show_field', locals: { question: next_question, prev_answer: })
-      end
+      render turbo_stream: next_question_streams(current_question, next_question)
     else
-      streams << turbo_stream.append('survey-profiles', partial: 'show_submit')
+      render turbo_stream: [
+        turbo_stream.append(current_question, partial: 'hide_question', locals: { question: current_question }),
+        turbo_stream.append('survey-profiles', partial: 'show_submit')
+      ]
     end
-
-    render turbo_stream: streams
   end
 
   def back
@@ -35,5 +27,22 @@ class Surveys::QuestionsController < ApplicationController
       turbo_stream.append(current_question, partial: 'hide_question', locals: { question: current_question }),
       turbo_stream.append(prev_question, partial: 'show_question', locals: { question: prev_question })
     ]
+  end
+
+  private
+
+  def next_question_streams(current_question, next_question)
+    streams = [
+      turbo_stream.append(current_question, partial: 'hide_question', locals: { question: current_question }),
+      turbo_stream.append(next_question, partial: 'show_question', locals: { question: next_question })
+    ]
+
+    prev_answer = params[:answers].fetch(params[:id])[:value]
+    partial_name = "show_#{next_question.answer_component_type}_field"
+    if template_exists?("/surveys/questions/#{partial_name}", [], true)
+      streams << turbo_stream.append(next_question, partial: partial_name, locals: { question: next_question, prev_answer: })
+    end
+
+    streams
   end
 end
