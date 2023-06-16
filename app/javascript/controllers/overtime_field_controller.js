e2b24bc53cfa298b9c94006602d90f0cbaa8de72
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-06-16 21:00:19 +0900

    SafariでEnterキーを押下したときフォーム送信されないように対応した

--- app/javascript/controllers/overtime_field_controller.js
+++ app/javascript/controllers/overtime_field_controller.js
@@ -6,4 +6,9 @@ export default class extends Controller {
   selectAll() {
     this.overtimeTarget.select()
   }
+
+  implicitSubmit() {
+    // Note: Enter キー押下でのフォーム送信（戻るボタン経由）を拒否する
+    // See: https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#implicit-submission
+  }
 }
