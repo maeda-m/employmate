a7f534abf9406bbd00130cddb4ca01de4a0a7ba3
Minoru Maeda <943541+maeda-m@users.noreply.github.com>
2023-06-18 20:54:07 +0900

    rails-i18n の ja.yml をできるだけ使用するようにした

--- app/views/components/day_of_week_field/component.rb
+++ app/views/components/day_of_week_field/component.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class DayOfWeekField::Component < RadioButtonField::Component
-  CAPTIONS = %w[日 月 火 水 木 金 土].freeze
-
   def choices
+    captions = I18n.translate('date.abbr_day_names')
     UnemploymentCertificationDay::CALENDAR_DAYS_OF_WEEK.map do |day_of_week|
-      [day_of_week, CAPTIONS[day_of_week]]
+      [day_of_week, captions[day_of_week]]
     end
   end
 end
